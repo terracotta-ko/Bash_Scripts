16fe1ff005d5fe19b8079e56ef951f5bf3f3654f
Kobe <terracotta711@gmail.com>
2018-04-23 11:55:37 +0800

    update go_config

--- installScript/go_config.sh
+++ installScript/go_config.sh
@@ -1,11 +1,11 @@
 #! /bin/bash
 
-mkdir ~/Go
+mkdir -p ~/Tools/golang
 
 BASHRC="$HOME/.bashrc"
 
 echo "# go setting" >> $BASHRC
-echo "export GOPATH=\$HOME/Go" >> $BASHRC
+echo "export GOPATH=\$HOME/Tools/golang" >> $BASHRC
 echo "export GOBIN=\$GOPATH/bin" >> $BASHRC
 echo "export GOROOT=/usr/local/opt/go/libexec" >> $BASHRC
 echo "export PATH=\$PATH:\$GOPATH/bin:\$GOROOT/bin" >> $BASHRC
